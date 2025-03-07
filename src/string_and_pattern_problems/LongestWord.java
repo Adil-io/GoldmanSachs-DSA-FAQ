0273ee0325119729e4d9ea36ecb8098d589957f4
adil_yusuf <adil_yusuf@epam.com>
2025-03-07 18:27:44 +0530

    Fixed LongestWord

--- src/string_and_pattern_problems/LongestWord.java
+++ src/string_and_pattern_problems/LongestWord.java
@@ -1,25 +1,26 @@
 package string_and_pattern_problems;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Problem Statement
- *  Given a string of letters and a dictionary, the function longestWord should
- *  find the longest word or words in the dictionary that can be made from the letters
+ * Given a string of letters and a dictionary, the function longestWord should
+ * find the longest word or words in the dictionary that can be made from the letters
  * Input: letters = "oet", dictionary = {"to","toe","toes"}
  * Output: {"toe"}
- *  Only lowercase letters will occur in the dictionary and the letters
- *  The length of letters will be between 1 and 10 characters
- *  The solution should work well for a dictionary of over 100,000 words
+ * Only lowercase letters will occur in the dictionary and the letters
+ * The length of letters will be between 1 and 10 characters
+ * The solution should work well for a dictionary of over 100,000 words
  * Signature- Set<String> longestWord(String letters, Dictionary dictionary) {}
  * Test Cases
- *  Dictionary dict = new Dictionary(new String[]{"to", "toe", "toes", "doe", "dog", "god", "dogs", "book", "banana"});
- *  Input: toe
- *  Output: toe
- *  Input – oetdg
- *  Output- "doe", "toe", "dog", "god"
+ * Dictionary dict = new Dictionary(new String[]{"to", "toe", "toes", "doe", "dog", "god", "dogs", "book", "banana"});
+ * Input: toe
+ * Output: toe
+ * Input – oetdg
+ * Output- "doe", "toe", "dog", "god"
  */
 public class LongestWord {
 
@@ -27,7 +28,7 @@ public class LongestWord {
     }
 
     public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary(new String[]{"to", "toe", "toes", "doe", "dog", "god", "dogs", "book", "banana"});
+        Dictionary dictionary = new Dictionary(new String[]{"to", "toe", "toes", "ot", "doe", "dog", "god", "dogs", "book", "banana"});
         String letters = "oetdg";
         System.out.println(longestWord(letters, dictionary));
     }
@@ -41,24 +42,29 @@ public class LongestWord {
                 if (length > maxLength) {
                     maxLength = length;
                     longestWords.clear();
+                    longestWords.add(str);
+                } else if (length == maxLength) {
+                    longestWords.add(str);
                 }
-                longestWords.add(str);
             }
         }
         return longestWords;
     }
 
     private static boolean canForm(String letters, String word) {
-        Set<Character> set = new HashSet<>();
-        for (char ch : letters.toCharArray()) {
-            set.add(ch);
-        }
+        List<Character> wordCharList = new ArrayList<>();
         for (char ch : word.toCharArray()) {
-            if (!set.contains(ch)) {
-                return false;
+            wordCharList.add(ch);
+        }
+
+        for (char ch : letters.toCharArray()) {
+            int i = wordCharList.indexOf(ch);
+            if (i >= 0) {
+                wordCharList.remove(i);
             }
         }
-        return true;
+
+        return wordCharList.isEmpty();
     }
 
 }
